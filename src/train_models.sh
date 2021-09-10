6cbfe89e3d2774c1dc64f6de809f3b7659c17711
TeddyGlass <kota.kurosaki0226@gmail.com>
2021-09-10 17:28:47 +0900

    complete pipeline to build stacking model

--- src/train_models.sh
+++
@@ -1,5 +0,0 @@
-# 20210907
-config=pj20210907_settings.ini
-python ./pipeline/train_models.py $config --LGB --use_best_params
-python ./pipeline/train_models.py $config --XGB --use_best_params
-python ./pipeline/train_models.py $config --NN --use_best_params
